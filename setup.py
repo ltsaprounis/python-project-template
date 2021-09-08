d858cad91c5cb19d78392e26711b51bdd9953072
ltsaprounis <leonidas.tsap@gmail.com>
2021-09-08 23:26:48 +0100

    setup.py formatting

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setup(
     packages=find_packages(),
     description="package description",
     author="author name",
-    install_requires=["numpy>=1.0", "pandas==1.0"],
+    install_requires=[
+        "numpy>=1.0",
+        "pandas==1.0",
+    ],
 )
